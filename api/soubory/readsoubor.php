eac0193c1e57b11f84519262c7844cf7507a2b07
Michael Ulmann <mixula7@gmail.com>
2025-07-12 21:26:23 +0200

    Added video file name

--- api/soubory/readsoubor.php
+++ api/soubory/readsoubor.php
@@ -12,10 +12,12 @@ class VideoStream
     private $start  = -1;
     private $end    = -1;
     private $size   = 0;
+    private $fileName = "";
  
-    function __construct($filePath) 
+    function __construct($filePath, $fileName) 
     {
         $this->path = $filePath;
+        $this->fileName = $fileName;
     }
      
     /**
@@ -36,6 +38,7 @@ class VideoStream
     {
         ob_get_clean();
         header("Content-Type: video/mp4");
+        header("Content-Disposition: inline; filename=\"".$this->fileName."\"");
         header("Cache-Control: max-age=2592000, public");
         header("Expires: ".gmdate('D, d M Y H:i:s', time()+2592000) . ' GMT');
         header("Last-Modified: ".gmdate('D, d M Y H:i:s', @filemtime($this->path)) . ' GMT' );
@@ -148,7 +151,7 @@ if(!file_exists($path)){
 }
 
 if($mimeType == "video/mp4") {
-  $stream = new VideoStream($path);
+  $stream = new VideoStream($path, $name);
   $stream->start();
   exit;
 }
